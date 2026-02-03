3c074ce805efe3832d3f1e0edb36d6818e115dbe
Awais Ahmed Khan <45173198+AwaisKhan128@users.noreply.github.com>
2026-02-03 16:06:08 +0500

    Update constants.dart

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,4 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
-const String newsAPIKey = '7c728f08eed148b0aa6e540c46c4a047';
+const String newsAPIKey = 'xxxxxxx';
 const String country = 'us';
 const String categoryQuery = "general";
